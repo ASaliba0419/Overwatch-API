2be79bfb4d330be62d3c8ecdff224e1a41f4829c
Kraaackle <saliba.andrew@outlook.com>
2021-08-20 14:29:51 -0400

    Updated Logo

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import Header from './components/Header'
 
 function App() {
   return (
-  <h1>Hello, first react project</h1>
+    <div className='center'>
+      <Header />
+      </div>
   );
 }
 
