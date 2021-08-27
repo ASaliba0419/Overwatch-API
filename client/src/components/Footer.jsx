53653ca58f838f2d60c5ced9824a1c2ae1c3ce13
Kraaackle <saliba.andrew@outlook.com>
2021-08-27 11:16:06 -0400

    Made adjustments to Footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -7,12 +7,12 @@ const Footer = () => {
 
   return (
     <>
-
+  <>
     <div className="footer">
         <h5>Andrew Saliba </h5>
         <a href="https://github.com/ASaliba0419" target='_blank'><img src={GitHub} alt="Git Hub"/></a>
         </div>
-
+</>
         </>
   )
 }
