3c25ac54a7bf14ae5aab2d21292152c986512f40
Kraaackle <saliba.andrew@outlook.com>
2021-08-27 11:20:40 -0400

    Made adjustments to Footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -9,8 +9,7 @@ const Footer = () => {
     <>
   <>
     <div className="footer">
-        <h5>Andrew Saliba </h5>
-        <a href="https://github.com/ASaliba0419" target='_blank'><img src={GitHub} alt="Git Hub"/></a>
+        <h6 className='footer2'>Andrew Saliba <br /><a href="https://github.com/ASaliba0419" target='_blank'><img src={GitHub} alt="Git Hub"/></a>All right go to Blizzard Studios</h6>
         </div>
 </>
         </>
